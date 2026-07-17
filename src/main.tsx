f389a637c0bd9a9b75bb1c85cc93cef943168717
RDaneliya <rdaneliya@yandex.ru>
2026-07-17 18:33:47 +0400

    Self-host Exo 2 and JetBrains Mono instead of Google Fonts.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App'
 import { ErrorBoundary } from './app/ErrorBoundary'
+import './styles/fonts.css'
 import './styles/global.css'
 
 createRoot(document.getElementById('root')!).render(
